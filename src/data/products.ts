699a4631455e9cacd977d9bf219d91da21f278b2
itismeherb <mikolajlaskowski7@gmail.com>
2026-01-08 22:58:28 +0100

    product updates

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,21 +6,27 @@ export const products = [
     quantity: 50,
     quantity_unit: "ml",
     image: "/images/masc_z_wrotyczu.jpg",
-    description: `
-    🌿 NATURALNA MAŚĆ Z WROTYCZU 🌿
+    description: `Naturalna maść z wrotyczu
 
-    Masz problem z suchą, podrażnioną lub problematyczną skórą?
-    Postaw na sprawdzony, domowy wyrób z natury 🌱
-
-    ✨ Dlaczego warto?
-    ✔️ wspiera regenerację skóry
-    ✔️ łagodzi podrażnienia i zaczerwienienia
-    ✔️ polecana przy egzemie, łuszczycy i trądziku
-    ✔️ naturalnie odstrasza owady 🦟
-    ✔️ wykonana z wrotyczu zbieranego w czystym, leśnym otoczeniu
+Masz problem z suchą, podrażnioną lub problematyczną skórą?
+Postaw na sprawdzony, domowy wyrób z natury.`,
+    sklad: `
+• macerat z wrotyczu
+• olej roślinny
+• wosk pszczeli
+    `,
+    zastosowanie: `
+Stosować zewnętrznie na skórę.
+Niewielką ilość nanieść na miejsce wymagające pielęgnacji 1–2 razy dziennie.
+    `,
+    dzialanie: `
+• wspiera regenerację skóry
+• łagodzi podrażnienia i zaczerwienienia
+• polecana przy egzemie, łuszczycy i trądziku
+• naturalnie odstrasza owady
     `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/masc-z-wrotyczu-50-ml-ostatnie-sztuki-CID3647-ID17ylhD.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/masc-z-wrotyczu-50-ml-ostatnie-sztuki-CID3647-ID17ylhD.html",
   },
   {
     id: "syrop-z-wrotyczu-nieslodzony",
@@ -29,18 +35,24 @@ export const products = [
     quantity: 200,
     quantity_unit: "ml",
     image: "/images/syrop_z_wrotyczu_nieslodzony.jpg",
-    description: `
-      🌿 NIESŁODZONY SYROP Z WROTYCZU – 200 ml 🌿
-      Naturalny wyrób z wrotyczu zbieranego w naszym ekologicznym gospodarstwie, położonym wśród lasów, z dala od pól uprawnych i oprysków 🌱
-      ✨ Dlaczego warto?
-      ✔️ tradycyjnie stosowany w zielarstwie
-      ✔️ wykorzystywany głównie zewnętrznie
-      ✔️ ceniony za właściwości przeciwzapalne i przeciwbólowe
-      ✔️ może być stosowany do okładów, nacierań oraz przemywania skóry
-      ✔️ niesłodzony, bez zbędnych dodatków
-      `,
+    description: `Niesłodzony syrop z wrotyczu – 200 ml
+
+Naturalny wyrób z wrotyczu zbieranego w ekologicznym gospodarstwie.`,
+    sklad: `
+• wrotycz
+• woda
+    `,
+    zastosowanie: `
+Do użytku zewnętrznego.
+Stosować do okładów, nacierań i przemywania skóry.
+    `,
+    dzialanie: `
+• działa przeciwzapalnie
+• łagodzi ból
+• wspiera regenerację skóry
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/syrop-z-wroryczu-CID3647-ID18UVVn.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/syrop-z-wroryczu-CID3647-ID18UVVn.html",
   },
   {
     id: "domowy-syrop-mietowy",
@@ -49,22 +61,24 @@ export const products = [
     quantity: 330,
     quantity_unit: "ml",
     image: "/images/domowy_syrop_mietowy.jpg",
-    description: `
-  🌿 DOMOWY SYROP MIĘTOWY – 330 ml 🌿
+    description: `Domowy syrop miętowy – 330 ml
 
-  Naturalny, aromatyczny syrop miętowy przygotowany z mięty zbieranej
-  na łąkach Beskidu Niskiego 🌱
-  Idealny do napojów, deserów i na chłodne wieczory – prosto z natury!
-
-  ✨ Dlaczego warto?
-  ✔️ wspiera trawienie i pomaga przy wzdęciach
-  ✔️ działa orzeźwiająco i uspokajająco
-  ✔️ łagodzi kaszel, katar i podrażnienia gardła
-  ✔️ bogaty w naturalne antyoksydanty
-  ✔️ intensywny, świeży miętowy smak dzięki mentolowi
-  `,
+Naturalny, aromatyczny syrop miętowy przygotowany z mięty zbieranej w Beskidzie Niskim.`,
+    sklad: `
+• mięta
+• cukier
+• woda
+    `,
+    zastosowanie: `
+Dodatek do wody, herbaty, deserów i napojów.
+    `,
+    dzialanie: `
+• wspiera trawienie i pomaga przy wzdęciach
+• działa orzeźwiająco i uspokajająco
+• łagodzi kaszel i podrażnienia gardła
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/syrop-mietowy-330-ml-CID757-ID17ye9t.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/syrop-mietowy-330-ml-CID757-ID17ye9t.html",
   },
   {
     id: "syrop-z-mlodych-pedow-sosny",
@@ -73,21 +87,23 @@ export const products = [
     quantity: 330,
     quantity_unit: "ml",
     image: "/images/syrop_z_mlodych_pedow_sosny.jpg",
-    description: `
-  🌲 SYROP Z MŁODYCH PĘDÓW SOSNY – 330 ml 🌲
+    description: `Syrop z młodych pędów sosny – 330 ml
 
-  Naturalny, nierozcieńczany syrop z młodych pędów sosny,
-  zbierany w głębokim lesie, z dala od działalności ludzkiej 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ działa wykrztuśnie – pomaga przy kaszlu i zalegającym śluzie
-  ✔️ łagodzi stany zapalne dróg oddechowych i gardła
-  ✔️ wspiera odporność dzięki witaminie C, flawonoidom i minerałom
-  ✔️ działa antybakteryjnie i przeciwwirusowo
-  ✔️ wspomaga trawienie i może łagodzić podrażnienia skóry
-  `,
+Naturalny syrop wspierający drogi oddechowe.`,
+    sklad: `
+• młode pędy sosny
+• cukier
+    `,
+    zastosowanie: `
+Dodatek do herbaty lub spożycie bezpośrednie.
+    `,
+    dzialanie: `
+• wspiera odporność
+• działa wykrztuśnie
+• łagodzi stany zapalne gardła
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/syrop-z-mlodych-pedow-sosny-330-ml-ostatnie-sztuki-CID757-ID17ydSH.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/syrop-z-mlodych-pedow-sosny-330-ml-ostatnie-sztuki-CID757-ID17ydSH.html",
   },
   {
     id: "syrop-z-nawloci-nieslodzony",
@@ -96,22 +112,23 @@ export const products = [
     quantity: 500,
     quantity_unit: "ml",
     image: "/images/syrop_z_nawloci_nieslodzony.jpg",
-    description: `
-  🌿 NIESŁODZONY SYROP Z NAWŁOCI – 500 ml 🌿
+    description: `Niesłodzony syrop z nawłoci – 500 ml
 
-  Naturalny syrop z nawłoci zbieranej w naszym ekologicznym
-  gospodarstwie, położonym wśród lasów, z dala od pól uprawnych 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ działa moczopędnie i wspiera oczyszczanie organizmu
-  ✔️ łagodzi stany zapalne i infekcje dróg moczowych
-  ✔️ wspomaga układ pokarmowy – niestrawność, wzdęcia, biegunki
-  ✔️ działanie antybakteryjne i przeciwgrzybiczne
-  ✔️ wspiera regenerację skóry i gojenie ran
-  ✔️ może pomóc w obniżaniu ciśnienia i rozkurczu mięśni gładkich
-  `,
+Naturalny syrop z nawłoci, zbieranej ekologicznie.`,
+    sklad: `
+• nawłoć
+• woda
+    `,
+    zastosowanie: `
+Stosować jako dodatek do napojów lub przemywania skóry.
+    `,
+    dzialanie: `
+• wspiera oczyszczanie organizmu
+• łagodzi stany zapalne dróg moczowych
+• wspomaga układ pokarmowy
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/syrop-z-nawloci-nieslodzony-CID757-ID17ylyV.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/syrop-z-nawloci-nieslodzony-CID757-ID17ylyV.html",
   },
   {
     id: "masc-z-krwawnika-na-oliwie-extra-virgin",
@@ -120,26 +137,24 @@ export const products = [
     quantity: 90,
     quantity_unit: "ml",
     image: "/images/masc_z_krwawnika.jpg",
-    description: `
-  🌼 MAŚĆ Z KRWAWNIKA NA OLIWIE EXTRA VIRGIN – 90 ml 🌼
+    description: `Maść z krwawnika na oliwie extra virgin – 90 ml
 
-  Naturalna maść z krwawnika przygotowana na bazie oliwy
-  extra virgin – idealnie wchłaniającej się w skórę 🌱
-
-  Zawiera również wosk pszczeli oraz ekstrakt z krwawnika
-  zbieranego w naszym ekologicznym polu, z dala od oprysków
-  i chemii.
-
-  ✨ Dlaczego warto?
-  ✔️ wspomaga gojenie ran, otarć i skaleczeń
-  ✔️ pielęgnuje suchą i popękaną skórę (np. dłonie, pięty)
-  ✔️ łagodzi stany zapalne, podrażnienia i objawy łuszczycy, egzemy czy trądziku
-  ✔️ przynosi ulgę w bólu mięśni i stawów, stłuczeniach i urazach
-  ✔️ działa kojąco przy ukąszeniach owadów
-  ✔️ regeneruje, nawilża i oczyszcza skórę
-  `,
+Naturalna maść do pielęgnacji skóry.`,
+    sklad: `
+• krwawnik
+• oliwa extra virgin
+• wosk pszczeli
+    `,
+    zastosowanie: `
+Nanieść na skórę 1–2 razy dziennie.
+    `,
+    dzialanie: `
+• wspomaga gojenie ran
+• łagodzi podrażnienia i stany zapalne
+• pielęgnuje suchą skórę
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/masc-z-krwawnika-90-ml-ev-CID3647-ID181J37.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/masc-z-krwawnika-90-ml-ev-CID3647-ID181J37.html",
   },
   {
     id: "masc-z-krwawnika-na-oleju-kokosowym",
@@ -148,24 +163,24 @@ export const products = [
     quantity: 90,
     quantity_unit: "ml",
     image: "/images/masc_z_krwawnika.jpg",
-    description: `
-  🌴 MAŚĆ Z KRWAWNIKA NA OLEJU KOKOSOWYM – 90 ml 🌴
+    description: `Maść z krwawnika na oleju kokosowym – 90 ml
 
-  Naturalna maść z krwawnika przygotowana na bazie oleju
-  kokosowego, wzbogacona woskiem pszczelim oraz ekstraktem
-  z krwawnika zbieranego w naszym ekologicznym polu,
-  z dala od oprysków 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ wspomaga gojenie ran, otarć i skaleczeń
-  ✔️ regeneruje i nawilża suchą, popękaną skórę (np. dłonie i pięty)
-  ✔️ łagodzi stany zapalne, podrażnienia, łuszczycę, egzemę i trądzik
-  ✔️ przynosi ulgę w bólu mięśni i stawów oraz przy stłuczeniach
-  ✔️ działa kojąco przy ukąszeniach owadów
-  ✔️ oczyszcza i wspiera zdrowie skóry, także cery problematycznej
-  `,
+Naturalna maść odżywcza i regenerująca.`,
+    sklad: `
+• krwawnik
+• olej kokosowy
+• wosk pszczeli
+    `,
+    zastosowanie: `
+Nanieść na skórę problematyczną 1–2 razy dziennie.
+    `,
+    dzialanie: `
+• łagodzi podrażnienia
+• regeneruje i nawilża skórę
+• działa kojąco przy ukąszeniach owadów
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/masc-z-krwawnika-90-ml-o-k-CID3647-ID18O25E.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/masc-z-krwawnika-90-ml-o-k-CID3647-ID18O25E.html",
   },
   {
     id: "masc-z-wrotyczu-na-oleju-kokosowym",
@@ -174,24 +189,25 @@ export const products = [
     quantity: 90,
     quantity_unit: "ml",
     image: "/images/masc_z_wrotyczu_na_oleju_kokosowym.jpg",
-    description: `
-  🌿 MAŚĆ Z WROTYCZU NA OLEJU KOKOSOWYM – 90 ml 🌿
+    description: `Maść z wrotyczu na oleju kokosowym – 90 ml
 
-  Naturalna maść przygotowana na bazie oleju kokosowego
-  z dodatkiem wosku pszczelego i ekstraktu z wrotyczu,
-  pochodzącego z naszego ekologicznego gospodarstwa 🌱
-
-  Zbierana w otoczeniu lasów, z dala od oprysków i chemii.
-
-  ✨ Dlaczego warto?
-  ✔️ pielęgnuje suchą, podrażnioną i problematyczną skórę
-  ✔️ wspomaga regenerację drobnych pęknięć skóry
-  ✔️ działa przeciwzapalnie i antybakteryjnie
-  ✔️ łagodzi zaczerwienienia i podrażnienia
-  ✔️ naturalnie odstrasza owady
-  `,
+Naturalna maść regenerująca i kojąca.`,
+    sklad: `
+• wrotycz
+• olej kokosowy
+• wosk pszczeli
+    `,
+    zastosowanie: `
+Nanieść na skórę 1–2 razy dziennie.
+    `,
+    dzialanie: `
+• pielęgnuje suchą i podrażnioną skórę
+• wspomaga regenerację
+• działa przeciwzapalnie i antybakteryjnie
+• naturalnie odstrasza owady
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/mazidlo-z-wrotyczu-90-ml-CID757-ID18O2gj.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/mazidlo-z-wrotyczu-90-ml-CID757-ID18O2gj.html",
   },
   {
     id: "zywy-ocet-winogronowy-jasne-winogrona",
@@ -200,24 +216,24 @@ export const products = [
     quantity: 250,
     quantity_unit: "ml",
     image: "/images/zywy_ocet.jpg",
-    description: `
-  🍇 ŻYWY OCET WINOGRONOWY Z JASNYCH WINOGRON – 250 ml 🍇
+    description: `Żywy ocet winogronowy – 250 ml
 
-  Naturalny, niepasteryzowany ocet winogronowy, bogaty w witaminy,
-  minerały i polifenole 🌱
-  Idealny do kuchni, pielęgnacji i wsparcia zdrowia.
-
-  ✨ Dlaczego warto?
-  ✔️ wspiera układ krążenia – pomaga obniżać ciśnienie i cholesterol
-  ✔️ poprawia trawienie i metabolizm
-  ✔️ działa antyoksydacyjnie i przeciwzapalnie, spowalnia procesy starzenia
-  ✔️ stabilizuje poziom cukru we krwi
-  ✔️ działa przeciwbakteryjnie i przeciwgrzybiczo
-  `,
+Niepasteryzowany, bogaty w witaminy i minerały.`,
+    sklad: `
+• jasne winogrona
+• woda
+    `,
+    zastosowanie: `
+Do napojów, kuchni i pielęgnacji.
+    `,
+    dzialanie: `
+• wspiera krążenie i metabolizm
+• działa antyoksydacyjnie
+• stabilizuje poziom cukru we krwi
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/zywy-ocet-winogronowy-z-jasnych-winogron-250-ml-CID757-ID17ZcI2.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/zywy-ocet-winogronowy-z-jasnych-winogron-250-ml-CID757-ID17ZcI2.html",
   },
-
   {
     id: "zywy-ocet-dzikiej-rozy-z-miodem",
     name: "Żywy ocet z owoców dzikiej róży z miodem akacjowym",
@@ -225,23 +241,25 @@ export const products = [
     quantity: 250,
     quantity_unit: "ml",
     image: "/images/zywy_ocet.jpg",
-    description: `
-  🌹 ŻYWY OCET Z OWOCÓW DZIKIEJ RÓŻY Z MIODEM AKACJOWYM – 250 ml 🌹
+    description: `Żywy ocet z owoców dzikiej róży – 250 ml
 
-  Naturalny ocet z owoców dzikiej róży, słodzony miodem akacjowym,
-  dzięki czemu ma niski indeks glikemiczny – idealny dla diabetyków 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ źródło witaminy C o wysokiej wchłanialności (80–90%)
-  ✔️ działa przeciwzapalnie i przeciwbakteryjnie
-  ✔️ wspiera odporność i układ trawienny
-  ✔️ wspomaga oczyszczanie organizmu z toksyn
-  ✔️ może regulować poziom cukru we krwi i wspierać odchudzanie
-  `,
+Słodzony miodem akacjowym, niski indeks glikemiczny.`,
+    sklad: `
+• owoce dzikiej róży
+• miód akacjowy
+• woda
+    `,
+    zastosowanie: `
+Do picia lub jako dodatek do potraw.
+    `,
+    dzialanie: `
+• wspiera odporność
+• działa przeciwzapalnie
+• wspomaga oczyszczanie organizmu
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/zywy-ocet-z-owocow-dzikiej-rozy-z-miodem-akacjowym-CID3647-ID17Za0D.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/zywy-ocet-z-owocow-dzikiej-rozy-z-miodem-akacjowym-CID3647-ID17Za0D.html",
   },
-
   {
     id: "zywy-ocet-aksamitkowy",
     name: "Żywy ocet aksamitkowy",
@@ -249,23 +267,24 @@ export const products = [
     quantity: 250,
     quantity_unit: "ml",
     image: "/images/zywy_ocet.jpg",
-    description: `
-  🌼 ŻYWY OCET AKSAMITKOWY – 250 ml 🌼
+    description: `Żywy ocet aksamitkowy – 250 ml
 
-  Naturalny ocet z kwiatów aksamitki, własnej produkcji,
-  przygotowany z surowców zbieranych z dala od pól uprawnych i oprysków 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ działa antybakteryjnie, przeciwwirusowo i przeciwgrzybiczo
-  ✔️ wspiera układ nerwowy i poprawia kondycję wzroku
-  ✔️ działa przeciwzapalnie i przeciwpasożytniczo
-  ✔️ poprawia kondycję skóry – nadaje jej miękkość i redukuje przebarwienia
-  ✔️ wspiera układ moczowy i ochronę wątroby
-  `,
+Naturalny ocet z kwiatów aksamitki.`,
+    sklad: `
+• kwiaty aksamitki
+• woda
+    `,
+    zastosowanie: `
+Do picia lub zastosowań kosmetycznych.
+    `,
+    dzialanie: `
+• działa antybakteryjnie
+• wspiera układ nerwowy
+• poprawia kondycję skóry
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/zywy-ocet-aksamitkowy-250-ml-CID757-ID17ZaJJ.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/zywy-ocet-aksamitkowy-250-ml-CID757-ID17ZaJJ.html",
   },
-
   {
     id: "zywy-ocet-winogronowy-ciemne-winogrona",
     name: "Żywy ocet winogronowy z ciemnych winogron",
@@ -273,20 +292,22 @@ export const products = [
     quantity: 250,
     quantity_unit: "ml",
     image: "/images/zywy_ocet.jpg",
-    description: `
-  🍷 ŻYWY OCET WINOGRONOWY Z CIEMNYCH WINOGRON – 250 ml 🍷
+    description: `Żywy ocet winogronowy – 250 ml
 
-  Naturalny, niepasteryzowany ocet winogronowy, samodzielnej produkcji,
-  przygotowany z własnych, ekologicznych winogron 🌱
-
-  ✨ Dlaczego warto?
-  ✔️ działa przeciwutleniająco, przeciwbakteryjnie i przeciwgrzybiczo
-  ✔️ wspiera układ krążenia – obniża ciśnienie i cholesterol, wzmacnia naczynia krwionośne
-  ✔️ wspomaga trawienie i metabolizm
-  ✔️ stabilizuje poziom cukru we krwi
-  ✔️ spowalnia procesy starzenia organizmu i skóry
-  `,
+Niepasteryzowany ocet z ciemnych winogron.`,
+    sklad: `
+• ciemne winogrona
+• woda
+    `,
+    zastosowanie: `
+Do picia i gotowania.
+    `,
+    dzialanie: `
+• wspiera krążenie
+• działa antyoksydacyjnie
+• wspomaga metabolizm
+    `,
     shopUrl:
-      "https://www.olx.pl/d/oferta/zywy-ocet-winogronowy-z-ciemnych-winogron-250-ml-CID757-ID17ZcvU.html?reason=seller_listing",
+      "https://www.olx.pl/d/oferta/zywy-ocet-winogronowy-z-ciemnych-winogron-250-ml-CID757-ID17ZcvU.html",
   },
 ];
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,57 +1,89 @@
 import { useParams, Link } from "react-router-dom";
+import { useState } from "react";
 import { products } from "../data/products";
 
 export default function ProductPage() {
   const { id } = useParams();
   const product = products.find((p) => p.id === id);
+  const [tab, setTab] = useState<"sklad" | "zastosowanie" | "dzialanie">(
+    "sklad",
+  );
 
   if (!product) {
     return <p className="p-8 text-center text-red-500">Produkt nie istnieje</p>;
   }
 
+  const tabs = [
+    { key: "sklad", label: "Skład" },
+    { key: "zastosowanie", label: "Zastosowanie" },
+    { key: "dzialanie", label: "Działanie" },
+  ] as const;
+
   return (
     <div className="mx-auto max-w-6xl px-6 py-16">
       <div className="grid gap-12 md:grid-cols-2">
         {/* Image */}
-        <div className="overflow-hidden rounded-3xl bg-zinc-100 h-[500px] md:h-[600px]">
+        <div className="overflow-hidden rounded-3xl bg-zinc-100 h-[500px]">
           <img
             src={product.image}
             alt={product.name}
-            className="w-full h-full object-cover"
+            className="h-full w-full object-cover"
           />
         </div>
 
         {/* Content */}
-        <div className="flex flex-col justify-start gap-4">
-          <p className="text-sm uppercase tracking-widest text-zinc-400">
-            Produkt
-          </p>
+        <div className="flex flex-col gap-6">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-900">
+            {product.name}
+          </h1>
 
-          <h1 className="text-4xl font-bold tracking-tight">{product.name}</h1>
-
-          <p className="text-xl text-zinc-600">
+          <p className="text-xl text-zinc-500">
             {product.quantity}
             {product.quantity_unit} · {product.price} zł
           </p>
 
-          <p className="whitespace-pre-line text-zinc-700 leading-relaxed">
-            {product.description}
-          </p>
+          <p className="text-zinc-500 leading-relaxed">{product.description}</p>
+
+          {/* Tabs */}
+          <div className="rounded-3xl border border-zinc-200 overflow-hidden bg-white">
+            {/* Tab headers */}
+            <div className="flex border-b border-zinc-200">
+              {tabs.map(({ key, label }) => (
+                <button
+                  key={key}
+                  onClick={() => setTab(key)}
+                  className={`flex-1 px-6 py-4 text-sm font-medium uppercase tracking-wide transition-colors ${
+                    tab === key
+                      ? "bg-zinc-50 text-zinc-900 border-b-2 border-zinc-900"
+                      : "text-zinc-500 hover:text-zinc-900 hover:bg-zinc-50"
+                  }`}
+                >
+                  {label}
+                </button>
+              ))}
+            </div>
+
+            {/* Tab content */}
+            <div className="p-6 min-h-[200px]">
+              <div className="whitespace-pre-line text-zinc-500 leading-relaxed">
+                {product[tab]}
+              </div>
+            </div>
+          </div>
 
           {/* Actions */}
-          <div className="mt-6 flex flex-col sm:flex-row gap-4">
+          <div className="mt-6 flex gap-4">
             <a
               href={product.shopUrl}
               target="_blank"
               rel="noopener noreferrer"
-              className="rounded-xl bg-black px-8 py-4 text-center text-white transition hover:bg-zinc-800"
+              className="flex-1 rounded-xl bg-black px-8 py-4 text-center text-sm font-medium text-white transition hover:bg-zinc-800"
             >
               Kup teraz
             </a>
-
             <Link
               to="/"
-              className="rounded-xl border border-zinc-300 px-8 py-4 text-center transition hover:bg-zinc-100"
+              className="rounded-xl border border-zinc-300 px-8 py-4 text-center text-sm font-medium transition hover:bg-zinc-100"
             >
               Wróć
             </Link>
